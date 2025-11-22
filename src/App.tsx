0e093ecba5a9737c9f045490e8b707aab8a93c90
Dabin Jeong <41@b41.kr>
2025-11-23 01:07:55 +0900

    chore: 리엑트 쿼리 클라이언트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { GlobalPortal, GlobalStyles } from 'tosslib';
+import { queryClient } from './lib/queryClient';
 import { Routes } from './pages/Routes';
 
 export function App() {
   return (
     <>
       <GlobalStyles />
-      <GlobalPortal.Provider>
-        <Routes />
-      </GlobalPortal.Provider>
+      <QueryClientProvider client={queryClient}>
+        <GlobalPortal.Provider>
+          <Routes />
+        </GlobalPortal.Provider>
+      </QueryClientProvider>
     </>
   );
 }
